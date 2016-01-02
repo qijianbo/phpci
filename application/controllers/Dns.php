da0394aca333aa1d8662f3cc67fc9654c56e5b74
Jianbo Qi <jianbo.qi@ele.me>
2016-01-02 20:32:20 +0800

    fix

--- application/controllers/Dns.php
+++ application/controllers/Dns.php
@@ -14,11 +14,24 @@ class Dns extends Ci_Controller{
         $data['my_list'] = $file;
     	$this->load->view('Dns/changes',$data);
     }
-    public function editnamed($item)
+    public function viewnamed($item)
     {
         # code...
         $myfile = fopen("/vagrant/named/$item", "r") or die("Unable to open file!");
-        echo fread($myfile,filesize("/vagrant/named/$item"));
+        $data = fread($myfile,filesize("/vagrant/named/$item"));
+        // return $data;
+        // print_r($data);
+        $date = nl2br("$data.\n");
+        // echo $date;
+        $this->load->view('Dns/viewnamed',$date);
+        fclose($myfile);
+    }
+    public function editnamed($item)
+    {
+        # code...
+        $myfile = fopen("/vagrant/named/$item", "rw") or die("Unable to open file!");
+        $data = fread($myfile,filesize("/vagrant/named/$item"));
+        echo nl2br("$data.\n");
         fclose($myfile);
     }
 }
