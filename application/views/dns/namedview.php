c8ee9c6038316c656d95e0e33066485e493b8f19
Jianbo Qi <jianbo.qi@ele.me>
2016-01-02 20:30:18 +0800

    fix

---
+++ application/views/dns/namedview.php
@@ -0,0 +1,5 @@
+
+<b><?php echo $date;?></b>
+<!-- // <b><a href="<?php echo '/dns/viewnamed/' .$item; ?>"> view </a></b> -->
+<!-- <b><a href="<?php echo '/dns/editnamed/' .$item; ?>"> edit </a></b> -->
+<!--<?php endforeach; ?>-->
---
+++ application/views/dns/viewnamed.php
@@ -0,0 +1,6 @@
+
+<?php foreach($list as $item):?>
+<b><?php echo $item;?></b>
+<!-- // <b><a href="<?php echo '/dns/viewnamed/' .$item; ?>"> view </a></b> -->
+<!-- <b><a href="<?php echo '/dns/editnamed/' .$item; ?>"> edit </a></b> -->
+<?php endforeach; ?>
